dcc36296922c839843a874a22d4f4e17bc1eba39
anikets349 <anikets349@gmail.com>
2022-03-01 12:31:28 +0530

    Added code for Expt-8

---
+++ codes/exp-8.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(void) {
+	int pid = fork();
+	if (pid == 0) { 
+		// inside the first child
+		pid = fork(); // creates the second child or grandchild
+		if (pid == 0) {
+			// inside the second child
+			printf("Grandchild PID : %d\n", getpid());
+			printf("Child PID : %d\n", getppid());
+		}
+	}
+	else {
+		wait(NULL);
+		system("ps -o pid,ppid,stat,tty,command");
+	}
+	return 0; 
+}
