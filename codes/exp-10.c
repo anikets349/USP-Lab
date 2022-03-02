097a8bb965bbaf98b4d7787cb3c0ffc71d7029c3
anikets349 <anikets349@gmail.com>
2022-03-02 10:58:07 +0530

    Added error handling code and comments in Expt-10

--- codes/exp-10.c
+++ codes/exp-10.c
@@ -4,16 +4,23 @@
 #include<signal.h>
 #define INTERVAL 5
 
-void callme(int sig_no) {
-	alarm(INTERVAL);
+void callme(int sig_no) { // print the follwing messages on the reception of signal SIGALRM
+	printf("Time interval elapsed!\n");
 	printf("WAKEUP!\n");
 }
 
 int main(void) {
 	struct sigaction action;
-	action.sa_handler = callme;
-	sigaction(SIGALRM, &action, 0);
-	alarm(2);
-	sleep(5);
+	action.sa_handler = callme; // set signal handler routine
+	sigemptyset(&action.sa_mask); // clear all signals in mask
+	
+	// set course for handling SIGALRM
+	if (sigaction(SIGALRM, &action, 0) == -1) { // course could not be set
+		perror("sigaction");
+	}
+	else { // course set
+		alarm(INTERVAL); // set alarm of INTERVAL seconds 
+		pause(); // wait until signal SIGALRM is received
+	}
 	return 0;
 }
